6afb507c6510b63bd3ede716daae0f27b7c6459f
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-20 19:34:17 -0300

    services + logic to save user

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr'
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(
+    private builder: FormBuilder,
+    private toastr: ToastrService,
+    private service: AuthService,
+    private router: Router
+    ) {
+  }
 
+  registerForm = this.builder.group({
+    id: this.builder.control('', Validators.compose([
+      Validators.required,
+      Validators.minLength(5)
+    ])),
+    name: this.builder.control('', Validators.required),
+    password: this.builder.control('', Validators.compose([
+      Validators.required,
+      Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
+    ])),
+    email: this.builder.control('', Validators.compose([
+      Validators.email,
+      Validators.required
+    ])),
+    gender: this.builder.control('male'),
+    role: this.builder.control(''),
+    isActive: this.builder.control(false, Validators.required)
+  })
+
+  proceedRegistration() {
+    if (this.registerForm.valid) {
+      this.service.saveUser(this.registerForm.value).subscribe( (res => {
+        this.toastr.success("User saved!", "Contact admin to activate this account...")
+        this.router.navigate(['login'])
+      }))
+    }
+    else this.toastr.error("Please enter valid data")
+  }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  APIURL ='http://localhost:3000/user'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllUsers() {
+    return this.http.get(this.APIURL)
+  }
+
+  getUserById(id: any) {
+    // return this.http.get(this.APIURL + '/' + id)
+    return this.http.get(`${ this.APIURL }/${ id }`)
+  }
+
+  saveUser(data: any) {
+    return this.http.post(this.APIURL, data)
+  }
+
+  updateUser(id: any, data: any) {
+    return this.http.put(`${ this.APIURL }/${ id }`, data)
+  }
 }
+
